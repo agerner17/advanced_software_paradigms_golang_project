71330bcf409b0216f6b9e6833fd5e5ae72ee7993
agerner19 <agerner17@gwmail.gwu.edu>
2021-07-01 22:06:30 -0400

    Up_to_Auth_with_golang

--- main.go
+++ main.go
@@ -16,12 +16,13 @@ type Product struct {
 
 /* We will create our catalog of VR experiences and store them in a slice. */
 var products = []Product{
-Product{Id: 1, Name: "Java", Slug: "world-of-authcraft", Description : "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
-Product{Id: 2, Name: "C++", Slug: "ocean-explorer", Description : "Explore the depths of the sea in this one of a kind underwater experience"},
-Product{Id: 3, Name: "Python", Slug : "dinosaur-park", Description : "Go back 65 million years in the past and ride a T-Rex"},
-Product{Id: 4, Name: "Rust", Slug : "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
-Product{Id: 5, Name: "Javascript", Slug: "robin-hood", Description : "Pick up the bow and arrow and master the art of archery"},
-Product{Id: 6, Name: "GO", Slug: "real-world-vr", Description : "Explore the seven wonders of the world in VR"}}
+  Product{Id: 1, Name: "World of Authcraft", Slug: "world-of-authcraft", Description : "Battle bugs and protect yourself from invaders while you explore a scary world with no security"},
+  Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description : "Explore the depths of the sea in this one of a kind underwater experience"},
+  Product{Id: 3, Name: "Dinosaur Park", Slug : "dinosaur-park", Description : "Go back 65 million years in the past and ride a T-Rex"},
+  Product{Id: 4, Name: "Cars VR", Slug : "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+  Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description : "Pick up the bow and arrow and master the art of archery"},
+  Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description : "Explore the seven wonders of the world in VR"}}
+
 
 func main() {
   // Here we are instantiating the gorilla/mux router
