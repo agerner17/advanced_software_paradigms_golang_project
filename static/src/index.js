79bfbb651becd3cb801d039cc3b105bba10e2083
rjhear <72757968+rjhear@users.noreply.github.com>
2021-07-03 17:30:26 -0400

    Connect front and back

--- static/src/index.js
+++ static/src/index.js
@@ -20,11 +20,24 @@ const onRedirectCallback = appState => {
 
 // Wrap App in the Auth0Provider component
 // The domain and client_id values will be found in your Auth0 dashboard
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain="dev-8-p2-x5k.us.auth0.com"
+//     client_id="65IQG0uREZs0pS0rQg2cEjx45PsiCcW0"
+//     redirect_uri={window.location.origin}
+//     onRedirectCallback={onRedirectCallback}
+//   >
+//     <App />
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
+
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-8-p2-x5k.us.auth0.com"
-    client_id="65IQG0uREZs0pS0rQg2cEjx45PsiCcW0"
+    domain={config.domain}
+    client_id={config.clientId}
     redirect_uri={window.location.origin}
+    audience={config.audience}     // NEW - specify the audience value
     onRedirectCallback={onRedirectCallback}
   >
     <App />
